3a5695f9698a563b26296fe8b70597748a6cc703
Bogaert <bogaert@outlook.com>
2022-01-31 15:47:17 +0100

    first commit

---
+++ table/Genre.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Genre]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [action] VARCHAR(50) NOT NULL, 
+    [scienceFiction] VARCHAR(50) NOT NULL, 
+    [horreur] VARCHAR(50) NULL, 
+    [drame] VARCHAR(50) NULL, 
+    [comedie_romatique] VARCHAR(50) NULL
+
+)
