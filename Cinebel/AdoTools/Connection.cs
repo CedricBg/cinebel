b576780ee7420f7d6c873d796e6ecb26d2840904
Bogaert <bogaert@outlook.com>
2022-01-31 16:23:53 +0100

    Commit 4

---
+++ Cinebel/AdoTools/Connection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdoToolbox
+{
+    public class Connection
+    {
+        SqlConnection _connection;
+        public SqlConnection connection
+        {
+           private get { return _connection; }
+            set { _connection = value; }
+        }
+
+
+        public Connection(string connexionString)
+        {
+            connection = new SqlConnection(connexionString);
+        }
+
+        
+        public bool ExecuteNonQuery(Command cmd)
+        {
+            connection
+                using (SqlCommand cmd = connexion.CreateCommand())
+                {
+                    cmd.CommandText = $"UPDATE Produit SET QteStock = @QteStock Where IdProduit = '{nrProduit}';";
+                    connexion.Open();
+                    cmd.Parameters.AddWithValue("QteStock", QuantiteStock);
+                    int Qte = (int)cmd.ExecuteNonQuery();
+                    connexion.Close();
+
+                }
+
+        }
+
+        public object ExecuteScalar(Command cmd)
+        {
+            
+        }
+
+        public IEnumerable<T> ExecuteReader<T>(Command cmd)
+        {
+
+            //yield return ;
+            //Vous aurez besoin d'un delegate type Func
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetDataTable(Command cmd)
+        {
+            //return new DataTable();
+
+        }
+    }
+}
---
+++ Cinebel/Models/film.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinebel.Models
+{
+    public class film
+    {
+        public int Id { get; set; }
+        public int Genre { get; set; }
+        public int Realisateur { get; set; }
+        public int Scenariste { get; set; }
+        public string Synopsis { get; set; }
+        public string Titre { get; set; }
+        public string Affiche { get; set; }
+        public DateTime AnneeDeSortie { get; set; }
+
+
+    }
+}
